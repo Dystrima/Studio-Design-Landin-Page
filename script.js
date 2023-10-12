a8bf8f40a5da44383da46ad1fdf54e9fc9236b65
Dystrima <138579225+Dystrima@users.noreply.github.com>
2023-10-12 16:17:34 +0200

    Update

--- script.js
+++ script.js
@@ -127,3 +127,22 @@ form.addEventListener("click", (event) => {
     console.log(company.value);
   }
 });
+
+/* ///// SWIPER ///// */
+
+const swiper = new Swiper(".mySwiperLogo", {
+  loop: true,
+
+  slidesPerView: 1,
+
+  breakpoints: {
+    768: { slidesPerView: 2.5 },
+    1024: { slidesPerView: 3.5 },
+  },
+
+  autoplay: { delay: 2000 },
+
+  centeredSlides: true,
+
+  spaceBetween: 60,
+});
